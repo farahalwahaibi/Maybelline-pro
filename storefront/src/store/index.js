2208291fe5a26231d831b148608511f5dde40cf9
farahalwahaibi <farah.alwahaibi77@gmail.com>
2021-07-05 03:24:31 +0300

    rendering

---
+++ storefront/src/store/index.js
@@ -0,0 +1,16 @@
+//create store 
+import { createStore, combineReducers } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import catReducer from './categories.js';
+import productReducer from './products.js';
+
+//combine reducer
+let reducers = combineReducers({ category: catReducer },{ product: productReducer });
+
+
+//create store
+const store = () => {
+    return createStore(reducers, composeWithDevTools());
+}
+
+export default store();
