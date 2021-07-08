5ff51780fe5ec91aec3d699676a9a40c24f1cbe1
farahalwahaibi <farah.alwahaibi77@gmail.com>
2021-07-08 06:42:45 +0300

    cart

--- src/components/cart/ShoppingCart.js
+++ src/components/cart/ShoppingCart.js
@@ -1,34 +1,67 @@
 import React from 'react';
+import {TextField,Button,Typography,Paper,Container,Grid} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
+const useStyles = makeStyles((theme) => ({
+    cardGrid: {
+      paddingTop: theme.spacing(9),
+      paddingBottom: theme.spacing(9),
+      marginLeft: '390px',
+      width: '550px'
+        },
+    card: {
+      display: 'flex',
+      flexDirection: 'column',
+    },
+    btn: {
+      textAlign: 'center',
+      marginLeft: '170px',
+          },
+    paper:{
+        width: '500px',
+        padding: theme.spacing(9),
+        // paddingBottom: theme.spacing(9),
+    }
+  }));
 
 const Cart = (props) => {
+    const classes = useStyles();
+
     return (
         <>
         <form>
+            <Container className={classes.cardGrid}>
+                <Paper className={classes.paper}>
             <h3>Order summary</h3>
             <h4>Total $0.00</h4>
-            <h4>Billing Address</h4>
-            <input type="text" placeholder="Full Name"></input>
+            <Grid className={classes.card}>
+            <Typography variant="h6">Billing Address</Typography>
+            <TextField type="text" label="Full Name"></TextField>
             <br></br>
-            <input type="text" placeholder="Address"></input>
+            <TextField type="text" label="Address"></TextField>
             <br></br>
-            <input type="text" placeholder="City"></input>
+            <TextField type="text" label="City"></TextField>
             <br></br>
-            <input type="text" placeholder="State"></input>
+            <TextField type="text" label="State"></TextField>
             <br></br>
-            <input type="text" placeholder="Zip"></input>
-            <br></br>
-            <h4>Payment details</h4>
-            <input type="number" placeholder="Credit Card #"></input>
-            <br></br>
-            <input type="date" placeholder="Expiration"></input>
-            <br></br>
-            <input type="" placeholder="CVV"></input>
+            <TextField type="text" label="Zip"></TextField>
             <br></br>
             <br></br>
-            <button type='submit'>Place Your Order</button>
+            </Grid>
+            <Grid className={classes.card}>
+            <Typography variant="h6">Payment details</Typography>
+            <TextField type="number" label="Credit Card #"></TextField>
             <br></br>
-            <br></br>   
+            <TextField type="date" label="Expiration" InputLabelProps={{shrink: true, }}></TextField>
+            <br></br>
+            <TextField type="" label="CVV"></TextField>
+            <br></br>
+            <br></br></Grid>
+            <Button variant="contained" color="primary" type='submit' className={classes.btn}>Place Your Order</Button>
+            <br></br>
+            <br></br>  
+            </Paper> 
+            </Container>
         </form>
         </>
     )
